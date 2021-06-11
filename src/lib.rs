73142dd8b186979f0c0512e0040a423750b05ee3
Hiroki Tokunaga <tokusan441@gmail.com>
2021-06-11 21:59:33 +0900

    feat: implement

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,37 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![no_std]
+
+use conquer_once::spin::Lazy;
+use core::fmt;
+use core::fmt::Write;
+use spinning_top::Spinlock;
+use uart_16550::SerialPort;
+
+static PORT: Lazy<Spinlock<SerialPort>> = Lazy::new(|| {
+    let mut port = unsafe { SerialPort::new(0x3f8) };
+
+    port.init();
+
+    Spinlock::new(port)
+});
+
+#[macro_export]
+macro_rules! qemu_print{
+    ($($arg:tt)*)=>{
+        $crate::_print(format_args!($($arg)*));
     }
+}
+
+#[macro_export]
+macro_rules! qemu_println {
+    () => {
+        $crate::qemu_print!("\n");
+    };
+    ($($arg:tt)*) => {
+        $crate::qemu_print!("{}\n", format_args!($arg));
+    };
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments<'_>) {
+    let _ = PORT.lock().write_fmt(args);
 }
